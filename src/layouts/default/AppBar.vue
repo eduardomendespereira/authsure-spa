f9fcdbbcfc63be22ce126c80510f0737d2eec54d
Gabriel Fidelis Pereira <83375130+FidelisGf@users.noreply.github.com>
2023-08-30 22:14:16 -0300

    nav and app-bar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,40 +1,91 @@
 <template>
-  <v-app-bar
-  color="gray"
-  absolute
->
-    <v-app-bar-nav-icon variant="text" 
-    @click.stop="drawer = !drawer">
+  <v-app-bar color="gray" absolute>
+    <v-app-bar-nav-icon variant="text" @click="expandNavBar = expandNavBar ? false : true">
     </v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-row>
-      <v-btn  variant="text" size="large" 
-        icon="mdi-help-circle-outline"></v-btn>
-      <v-btn class="ml-n3" variant="text" size="large" 
-      icon="mdi-account-circle-outline"></v-btn>
-      <p class="text-body">Admin</p>
-      <v-btn variant="text" size="large" 
-        icon="mdi-exit-to-app"
-        color="red">
-      </v-btn>
-    </v-row> 
+    <v-app-bar-title> AuthSure </v-app-bar-title>
+    <v-btn variant="text" size="large" icon="mdi-help-circle-outline"></v-btn>
+    <v-btn
+      variant="text"
+      size="large"
+      icon="mdi-account-circle-outline"
+    ></v-btn>
+    <v-btn variant="text" size="large" :to="'/login'" icon="mdi-exit-to-app" color="red">
+    </v-btn>
   </v-app-bar>
-
-  
+  <v-navigation-drawer
+      v-model="expandNavBar"
+      :temporary="smAndDown ? true : false"
+      :permanent="smAndDown ? false : true"
+    >
+    <v-list density="compact">
+      <v-list>
+        <v-list-item
+         
+          title="admin"
+        >
+        <template v-slot:prepend>
+          <v-icon size="large" icon="mdi-account-circle-outline"></v-icon>
+        </template>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list-subheader class="text-subtitle-1 mt-4" >Manage</v-list-subheader>
+      <v-divider class="mt-3"></v-divider>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :value="item"
+        class="mt-4"
+      >
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+        <template v-slot:title>
+          <p>{{item.text}}</p>
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+  import { ref } from 'vue';
+  import { useDisplay } from 'vuetify/lib/framework.mjs';
 
-  
+  const {
+    smAndDown
+  } = useDisplay()
+  const expandNavBar = ref(true)
 
-  const menuOptions = ref([
-      
-      {
-        title: "Sair",
-        icon: "mdi-exit"
-      }
-      
+  const items = ref([
+    {
+       text: "Clients",
+       icon: "mdi-application-brackets-outline"
+    },
+    {
+       text: "Realm roles",
+       icon: "mdi-account-hard-hat"
+    },
+    {
+       text: "Users",
+       icon: "mdi-account-cog"
+    },
+    {
+       text: "Groups",
+       icon: "mdi-account-group"
+    },
+    {
+       text: "Sessions",
+       icon: "mdi-sitemap-outline"
+    },
+    {
+       text: "Events",
+       icon: "mdi-post-outline"
+    },
   ])
-
 </script>
+<style scoped>
+.items {
+  padding-left: 10px !important;
+}
+</style>
