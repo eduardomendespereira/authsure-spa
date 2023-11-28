1afff45b200377ed0aaabf9ca0dd11436987c7e7
Gabriel Fidelis Pereira <83375130+FidelisGf@users.noreply.github.com>
2023-11-28 20:06:39 -0300

    Update variables.tf

--- terraform-ec2/variables.tf
+++ terraform-ec2/variables.tf
@@ -13,3 +13,7 @@ variable "public_key" {
 variable "key_name" {
   default = "auth_key_pair"
 }
+variable "AWS_REGION" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_TOKEN" {}
