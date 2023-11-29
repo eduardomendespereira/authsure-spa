48679642a815e85c4e4a212c6d29552a79d456a1
Gabriel Fidelis Pereira <83375130+FidelisGf@users.noreply.github.com>
2023-11-28 21:03:38 -0300

    Update main.tf

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state"
-    key            = "terraform.tfstate"
+    key            = "terraform2.tfstate"
     region         = "us-east-1"
     encrypt        = false
     dynamodb_table = "lock-table-name"
