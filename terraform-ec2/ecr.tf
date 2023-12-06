a1fd9b49723a90770103a3fa3366d9f4642eb781
Eduardo Mendes Pereira <84480565+eduardomendespereira@users.noreply.github.com>
2023-12-06 18:34:55 -0300

    Update ecr.tf

--- terraform-ec2/ecr.tf
+++ terraform-ec2/ecr.tf
@@ -1,3 +1,8 @@
-resource "aws_ecrpublic_repository" "foo" {
-  repository_name      = "authsure"
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
